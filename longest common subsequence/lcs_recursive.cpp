eb58b7e1f02b1319e0b7274819f85469daa71302
khandaker mushfiqur rahman <1505086.kmr@ugrad.cse.buet.ac.bd>
2020-07-14 23:57:34 +0600

    lcs string printed with backtracking

--- longest common subsequence/lcs_recursive.cpp
+++ longest common subsequence/lcs_recursive.cpp
@@ -4,6 +4,7 @@ using namespace std;
 string s1,s2;
 int mem[1002][1002];
 int lcs_recur(int i,int j){
+
     if(i==0 || j==0){
         mem[i][j]=0;
         ///cout<<i<<j<<endl;
@@ -21,6 +22,29 @@ int lcs_recur(int i,int j){
     }
 
 }
+
+string backtrack(int i,int j){
+
+    string lcs="";
+    while(i!=0 && j!=0){
+
+        if(mem[i][j]==1+mem[i-1][j-1] && s1[i-1]==s2[j-1]){
+            lcs=lcs+s1[i-1];
+            i--;
+            j--;
+        }
+        else if(mem[i][j]==mem[i-1][j]){
+            i=i-1;
+        }
+        else if(mem[i][j]==mem[i][j-1]){
+            j=j-1;
+        }
+
+    }
+    reverse(lcs.begin(),lcs.end());
+    return lcs;
+}
+
 int main(){
 //freopen("input.txt","r",stdin);
 //freopen("output.txt","w",stdout);
@@ -31,17 +55,20 @@ int tc;
 memset(mem,-1,sizeof(mem[0][0])*1002*1002);
 cin>>s1>>s2;
 cout<<s1.size()<<s2.size()<<endl;
+mem[0][0]=0;
 cout<<lcs_recur(s1.size(),s2.size())<<endl;
 
-/*for(int i=0;i<=s1.size();i++){
+for(int i=0;i<=s1.size();i++){
     for(int j=0;j<=s2.size();j++){
         cout<<mem[i][j]<<" ";
     }
     cout<<endl;
-}*/
-return 0;
 }
 
+cout<<backtrack(s1.size(),s2.size())<<endl;
+
+return 0;
+}
 /*
 sabsca
 asabcsa
